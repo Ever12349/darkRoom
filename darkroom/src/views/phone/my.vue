0846059ea705f4e9cf5430ffad4cbdfc5db4f68b
钱潇 <qianxiao@berightech.com>
2019-11-16 16:28:37 +0800

    临时提交

--- darkroom/src/views/phone/my.vue
+++ darkroom/src/views/phone/my.vue
@@ -1,15 +1,53 @@
 <template>
-    <div>
-        我的页面
+    <div class='my_bg'>
+        <!-- {{ message }} -->
+        <div class='my_header'>
+            
+        </div>
+        <div class='my_body'>
+
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-
-}
+    name:'my',
+    data(){
+        return {
+            message:'呵呵呵'
+        }
+    }
+};
 </script>
 
-<style>
+<style scoped>
+.my_bg {
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  top: 0;
+  left: 0;
+  background-image: url("../../assets/我的/my_background_2.jpg");
+  background-size:100%;
+  background-repeat: no-repeat;
+}
+.my_header{
+    width:100vw;
+    height:60vw;
+    
+}
+.my_body{
+    /* margin-top:50vw; */
+    width:100vw;
+    min-height:100vw;
+    background-color:#fff;
+    border-top-left-radius: 5vw;
+    border-top-right-radius: 5vw;
+}
+</style>
 
+
+
+<style>
 </style>
