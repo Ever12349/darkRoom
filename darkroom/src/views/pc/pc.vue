ea6a3671f094f6cc9424232d93a2dbd6578c8e01
钱潇 <1141831700@qq.com>
2020-01-08 20:30:09 +0800

    添加pc端缺省页

---
+++ darkroom/src/views/pc/pc.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="pc_bg">目前pc端未开发，请在移动端访问</div>
+</template>
+<style scoped>
+.pc_bg {
+  display: flex;
+  justify-content: center;
+  padding-top: 200px;
+  font-size: 25px;
+}
+</style>
